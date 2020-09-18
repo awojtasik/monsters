59999e3cd2934ddb1ea446bfbbfbe27312b8679b
Agnieszka Wojtasik <agnieszkawojtasik@MacBook-Air-Agnieszka.local>
2020-09-18 18:05:55 +0200

    Updates

---
+++ precache-manifest.dd4fc321cce667137a44323ffc19abec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33c77b442d4e05de0151da6d708adee",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "6432b2d6ef2ff819d140",
+    "url": "/monsters/static/css/main.7f5410fe.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/monsters/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6432b2d6ef2ff819d140",
+    "url": "/monsters/static/js/main.55672984.chunk.js"
+  },
+  {
+    "revision": "7d11275995119a716e88",
+    "url": "/monsters/static/js/runtime-main.5df1f7b5.js"
+  }
+]);
